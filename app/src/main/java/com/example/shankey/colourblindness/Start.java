a3276a27c9fbadcb64ad4d03516459d2cf592b6b
shankeymundey <shankeymundey085@gmail.com>
2019-04-11 23:55:05 +0530

    second commit

--- app/src/main/java/com/example/shankey/colourblindness/Start.java
+++ app/src/main/java/com/example/shankey/colourblindness/Start.java
@@ -1,6 +1,9 @@
 package com.example.shankey.colourblindness;
 
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +28,35 @@ public class Start extends AppCompatActivity {
 
         }
 
+
+
+
+
+    }
+    @Override
+    public  void onBackPressed(){
+        AlertDialog.Builder ob=new AlertDialog.Builder(this);
+
+        ob.setTitle("do you want to exit the test");
+        ob.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                 Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                  startActivity(intent);
+
+            }
+
+        });
+        ob.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        ob.create();
+        ob.show();
 
 
 
